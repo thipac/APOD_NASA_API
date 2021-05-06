59f3e97d6c9a95351b275b1ac427a23f49f0c20c
thipac <thiagolmcpacheco@gmail.com>
2021-05-06 00:39:31 -0300

    Entrega do projeto APOD

---
+++ nasa.js
@@ -0,0 +1,39 @@
+$('#date').on('change', function(){
+    
+    let date = $('#date').val()
+    const url = `https://api.nasa.gov/planetary/apod?api_key=mSQt4SNdemJT2Mwa7QIXhSff1RxejkQVumrUus8w&date=${date}`;
+
+    $.ajax({
+
+        'url': url,
+        'success':function(result){
+
+            $("div").removeClass('invi')
+
+            $('#title').html(`${result.title}`)
+
+            $('#texto').html(`${result.explanation}`)
+
+            if(result.media_type === "image"){
+
+            $('#conteudo__nasa').html(`
+            
+            <img class = "imagem" src = "${result.url}">
+            
+            `)
+
+            }
+
+            else if (result.media_type === "video"){
+
+                $('#conteudo__nasa').html(`
+            
+                <iframe class = "imagem" src = "${result.url}">
+            
+                `)
+            }
+            
+        },
+    })
+
+})
